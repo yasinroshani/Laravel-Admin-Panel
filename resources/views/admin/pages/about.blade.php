ab107b49d42caa4d2aef50718892aa0253294eed
yasinroshani <45326586+yasinroshani@users.noreply.github.com>
2019-08-30 16:26:01 +0430

    Create about.blade.php

---
+++ resources/views/admin/pages/about.blade.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Services | Roshani</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+    <div class="KW_progressContainer">
+      <div class="KW_progressBar">
+
+      </div>
+    </div>
+    <div class="page">
+    <nav id="colorlib-main-nav" role="navigation">
+      <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle active"><i></i></a>
+      <div class="js-fullheight colorlib-table">
+        <div class="img" style="background-image: url(images/author-2.jpg);"></div>
+        <div class="colorlib-table-cell js-fullheight">
+          <div class="row no-gutters">
+            <div class="col-md-12 text-center">
+              <h1 class="mb-4"><a href="index.html" class="logo">Mohammad Yasin Roshani</a></h1>
+              <ul>
+                <li><a href="index.html"><span><small>01</small>Home</span></a></li>
+                <li class="active"><a href="about.html"><span><small>02</small>Resume</span></a></li>
+                <li><a href="contact.html"><span><small>03</small>Contact</span></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    
+    <div id="colorlib-page">
+      <header>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="colorlib-navbar-brand">
+                <a class="colorlib-logo" href="index.html"><span class="logo-img" style="background-image: url(images/person_1.jpg);"></span>Mohammad Yasin Roshani</a>
+              </div>
+              <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <section class="ftco-section about-section">
+        <div class="container">
+          <div class="row d-flex my-5" data-scrollax-parent="true">
+            <div class="col-md-4 author-img" style="background-image: url(images/author-1.jpg);" data-scrollax=" properties: { translateY: '-70%'}"></div>
+            <div class="col-md-2"></div>
+            <div class="col-md-6 wrap ftco-animate">
+              <div class="about-desc">
+                <h1 class="bold-text">About</h1>
+                <div class="p-5">
+                  <h2 class="mb-5">Hi! I'm Mohammad Yasin Roshani</h2>
+                  <p>Web Developer (with technical expertise in developing websites, adept in web design projects; maintains existing websites and upgrades newly-acquired regularly with new applications as needed.</p>
+                  <p><a href="#">Checkout my resume</a></p>
+                  <ul class="ftco-footer-social list-unstyled mt-4">
+                    <li><a href="#"><span class="icon-twitter"></span></a></li>
+                    <li><a href="#"><span class="icon-facebook"></span></a></li>
+                    <li><a href="#"><span class="icon-instagram"></span></a></li>
+                  </ul>
+                  <h5>Contact me here!</h5>
+                  <p>Email: <a href="#">yasinroshani@yahoo.com</a></p>
+                  <p>Phone: <a href="#">+93 (0) 781 781 844</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row d-flex justify-content-end mt-5">
+          	<div class="col-md-10">
+          		<div class="profile ftco-animate">
+	          		<h2 class="mb-4">Profile</h2>
+	          		<h4 class="mb-4">I'm a UI/UX Designer &amp; Frontend Developer from Kabul, Afghanistan. I aim to make a difference through my creative solution.</h4>
+	          		<p>Technical expertise in developing websites, adept in web design projects; maintains existing websites and upgrades newly-acquired regularly with new applications as needed.</p>
+	          		<div class="row my-5">
+		          		<div class="col-md-6">
+		          			<p><strong>Name:</strong> <span>Mohammad Yasin Roshani</span></p>
+		          			<p><strong>Address:</strong> <span>Resalat Lane, District NO13, Kabul, Afghanistan</span></p>
+		          			<p><strong>Phone:</strong> <span>+93 (0) 781 781 844</span></p>
+		          		</div>
+
+	          		</div>
+          		</div>
+
+          		<div class="exp mt-5 ftco-animate">
+          			<h2 class="mb-4">Experience</h2>
+          			<div class="exp-wrap py-4">
+          				<div class="desc">
+	          				<h4>Develop Web Content Tasks &amp; Design Web Page Layout <span> - CodeWeekend</span> </h4>
+	          				<p class="location">Germany - Berlin</p>
+          				</div>
+          				<div class="year">
+          					<p>2019 - Present</p>
+          				</div>
+          			</div>
+          		</div>
+
+
+          		<div class="exp mt-5 ftco-animate">
+          			<h2 class="mb-4">Education</h2>
+          			<div class="exp-wrap py-4">
+          				<div class="desc">
+	          				<h4>Bacheclor Degree in Computer Science<span> - Avicenna University</span> </h4>
+	          				<p class="location">Afghanistan - Kabul</p>
+          				</div>
+          				<div class="year">
+          					<p>2015 - 2019</p>
+          				</div>
+          			</div>
+
+          			<div class="exp-wrap py-4">
+          				<div class="desc">
+	          				<h4>High School<span> - Babah High School</span> </h4>
+	          				<p class="location">Afghanistan - Jaghori, Ghazni</p>
+          				</div>
+          				<div class="year">
+          					<p>2002 - 2014</p>
+          				</div>
+          			</div>
+
+          			<div class="exp-wrap py-4">
+          				<div class="desc">
+	          				<h4>Full Stack Web Development<span> - CodeWeekend</span> </h4>
+	          				<p class="location">Afghanistan - Kabul</p>
+          				</div>
+          				<div class="year">
+          					<p>2019 - Present</p>
+          				</div>
+          			</div>
+
+          			
+          		</div>
+          	</div>
+          </div>
+          <div class="row mt-5 flex-column ftco-animate">
+          	<div class="col-md-8">
+          		<h2 class="mb-4">My Skills</h2>
+          	</div>
+          	<div class="col-md-6 animate-box">
+								<div class="progress-wrap">
+									<h4>Photoshop</h4>
+									<div class="progress">
+									 	<div class="progress-bar color-1" role="progressbar" aria-valuenow="75"
+									  	aria-valuemin="0" aria-valuemax="100" style="width:75%">
+									    <span>75%</span>
+									  	</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6 animate-box" data-animate-effect="fadeInRight">
+								<div class="progress-wrap">
+									<h4>jQuery</h4>
+									<div class="progress">
+									 	<div class="progress-bar color-1" role="progressbar" aria-valuenow="60"
+									  	aria-valuemin="0" aria-valuemax="100" style="width:60%">
+									    <span>60%</span>
+									  	</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6 animate-box">
+								<div class="progress-wrap">
+									<h4>HTML5</h4>
+									<div class="progress">
+									 	<div class="progress-bar color-1" role="progressbar" aria-valuenow="95"
+									  	aria-valuemin="0" aria-valuemax="100" style="width:95%">
+									    <span>95%</span>
+									  	</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6 animate-box" data-animate-effect="fadeInRight">
+								<div class="progress-wrap">
+									<h4>CSS3</h4>
+									<div class="progress">
+									 	<div class="progress-bar color-1" role="progressbar" aria-valuenow="90"
+									  	aria-valuemin="0" aria-valuemax="100" style="width:90%">
+									    <span>90%</span>
+									  	</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6 animate-box">
+								<div class="progress-wrap">
+									<h4>WordPress</h4>
+									<div class="progress">
+									 	<div class="progress-bar color-1" role="progressbar" aria-valuenow="70"
+									  	aria-valuemin="0" aria-valuemax="100" style="width:70%">
+									    <span>70%</span>
+									  	</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6 animate-box" data-animate-effect="fadeInRight">
+								<div class="progress-wrap">
+									<h4>MYSQL</h4>
+									<div class="progress">
+									 	<div class="progress-bar color-1" role="progressbar" aria-valuenow="90"
+									  	aria-valuemin="0" aria-valuemax="100" style="width:90%">
+									    <span>90%</span>
+									  	</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6 animate-box" data-animate-effect="fadeInRight">
+								<div class="progress-wrap">
+									<h4>Laravel</h4>
+									<div class="progress">
+									 	<div class="progress-bar color-1" role="progressbar" aria-valuenow="70"
+									  	aria-valuemin="0" aria-valuemax="100" style="width:70%">
+									    <span>70%</span>
+									  	</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6 animate-box" data-animate-effect="fadeInRight">
+								<div class="progress-wrap">
+									<h4>PHP</h4>
+									<div class="progress">
+									 	<div class="progress-bar color-1" role="progressbar" aria-valuenow="70"
+									  	aria-valuemin="0" aria-valuemax="100" style="width:70%">
+									    <span>70%</span>
+									  	</div>
+									</div>
+								</div>
+							</div>
+          </div>
+        </div>
+      </section>
+      
+      <footer class="ftco-footer ftco-bg-dark ftco-section">
+        <div class="container">
+          <div class="row mb-5 justify-content-center">
+            <div class="col-md-5 text-center">
+              <div class="ftco-footer-widget mb-5">
+                <ul class="ftco-footer-social list-unstyled">
+                  <li class="ftco-animate"><a href="https//www.twitter.com/yasinroshani"><span class="icon-twitter"></span></a></li>
+                  <li class="ftco-animate"><a href="https//www.facebook.com/yasinroshani"><span class="icon-facebook"></span></a></li>
+                  <li class="ftco-animate"><a href="https//www.instagram.com/yasinroshani"><span class="icon-instagram"></span></a></li>
+                </ul>
+              </div>
+              <div class="ftco-footer-widget">
+                <h2 class="mb-3">Contact Us</h2>
+                <p class="h3 email"><a href="#">yasinroshani@yahoo.com</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 text-center">
+
+              <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+      Copyright &copy;<script>document.write(new Date().getFullYear());</script> <a>by Mohammad Yasin Roshani</a>
+      <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+      <!-- loader -->
+      <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+      </div>
+
+    </div>
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/scrollax.min.js"></script>
+    <script src="js/bootstrap-datepicker.js"></script>
+    <script src="js/jquery.timepicker.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+    <script src="js/google-map.js"></script>
+    <script src="js/main.js"></script>
+    
+  </body>
+</html>
